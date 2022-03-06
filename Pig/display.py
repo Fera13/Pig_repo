59faecdd20f623e0f1fbb90e5b463fa24c9f7b02
Stuhren <alfredstyrenberg@gmail.com>
2022-03-06 16:44:27 +0100

    Added the display class

---
+++ Pig/display.py
@@ -0,0 +1,30 @@
+class Display:
+
+    def winner(self):
+        print(f'\n----------------------------------------------------------------------')
+        print(f'\n          Congratulations player, you have won the game!')
+        print(f'\n----------------------------------------------------------------------\n')
+    
+    
+    def gameMenu(self):
+        print('----------------------------------------------------------------------')
+        print(f'\nEnter one of the following options:\n')
+        print('1:  Play Pig-Game')
+        print('2:  Show the the rules of Pig-Game')
+        print('3:  Show the current highscores')
+        print('4:  Quit')
+        print(f'\n----------------------------------------------------------------------')
+        
+    def displayGameRules(self):
+        return self
+
+        
+    print('----------------------------------------------------------------------')
+    print(f'\nEnter one of the following options:\n')
+    print('1:  Play Pig-Game')
+    print('2:  Show the the rules of Pig-Game')
+    print('3:  Show the current highscores')
+    print('4:  Quit')
+    print(f'\n----------------------------------------------------------------------')
+    
+    
