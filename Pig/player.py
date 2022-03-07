24d6c66ba6a088ba549aa173171ea43de925f655
Emil <emil@gnaget.se>
2022-03-08 00:49:39 +0100

    changed setScore method to require both name and score, and added tests for setScore

--- Pig/player.py
+++ Pig/player.py
@@ -1,20 +1,19 @@
 class Player:
     names = []
-    score = 0
+    score = []
 
     def __init__(self):
         return None
 
 
-    def setName(self, name):
+    def setName(self, name: str):
         if name in self.names:
             print(name + " has already been set")
         else:
             self.names.append(name)
 
         return len(self.names)
-        
-        
+
         # call method to update name in highscore
 
 
@@ -22,8 +21,16 @@ class Player:
         return self.names
 
     
-    def setScore(self, score):
-        self.score = score
+    def setScore(self, name:str, score:int):
+        if not isinstance(name, str) or not isinstance(score, int):
+            raise TypeError("name has to be a string, and score has to be an integer")
+        
+        if score < 0:
+            raise ValueError("score cant be a negative value")
+
+        self.score.append([name, score])
+        return self.score
+            
 
 
     def getScore(self):
--- Pig/test_player.py
+++ Pig/test_player.py
@@ -4,6 +4,7 @@ from player import *
 player = Player()
 
 class testPlayer(unittest.TestCase):
+
     def test_setName_duplicates(self):
         self.assertEqual(player.setName("name1"), 1)
         self.assertEqual(player.setName("name1"), 1)
@@ -11,6 +12,14 @@ class testPlayer(unittest.TestCase):
         self.assertEqual(player.setName("name3"), 3)
         self.assertEqual(player.setName("name2"), 3)
 
+    def test_setScore_wrongType(self):
+        with self.assertRaises(TypeError): player.setScore("emil", "2")
+        with self.assertRaises(TypeError): player.setScore(2, "2")
+        with self.assertRaises(TypeError): player.setScore(2, 2)
+    
+    def test_setScore_negativeValue(self):
+        with self.assertRaises(ValueError): player.setScore("emil", -2)
+        with self.assertRaises(ValueError): player.setScore("emil", -1)
 
 if __name__ == "__main__":
     unittest.main()
