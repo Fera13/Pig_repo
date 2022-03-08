8ccf4b9e8511cbf996236bf4f52aa7c03d81cd62
Fera M <fera6055@gmail.com>
2022-03-08 17:21:02 +0100

    Updating functionality

--- Pig/Game_functionality.py
+++ Pig/Game_functionality.py
@@ -1,6 +1,8 @@
+import rlcompleter
 from High_score import *
 from display import *
 from player import *
+from dice import *
 
 currentPlayer = "player1's turn"
 currentPlayer1 = "player1's turn"
@@ -8,6 +10,7 @@ currentPlayer1 = "player1's turn"
 hs = High_score()
 disp = Display()
 playr = Player()
+dise = dice()
 
 class Game_functionality:
     
@@ -16,17 +19,20 @@ class Game_functionality:
             self.enter_Names1()
             # start game
         elif choice == 2:
-            self.enter_Names2()
+            self.enter_Names2p()
             # start game
         elif choice == 3:
-            #view player list
+            names = playr.getNames()
+            disp.showPlayers(names)
             name = input("Enter the name you want to update: ")
             newName = input("Enter the new updated name: ")
-            # method to switch names(name, newName)
+            playr.updateName(name, newName)
             hs.update_High_Score(name, newName)
         elif choice == 4:
-            name = input("Enter the name you want to delete: ")
-            #method of deleting a name from the list (name)
+            names = playr.getNames()
+            disp.showPlayers(names)
+            name = input("\nEnter the name you want to delete: ")
+            playr.deleteName(name)
         elif choice == 5:
             hsDic = {}
             hsDic == hs.get_HighScore_Dic()
@@ -37,23 +43,35 @@ class Game_functionality:
             quit()
     
     
-    def view_Game_Prog_2(player1: str, points1: int, player2: str, points2: int):
-        print(f"Player1: {player1:60} Player2: {player2}")
-        print(f"Points: {points1:<61} Points: {points2}\n")
-    
-    
-    def enter_Names2(self):
-        print("Existing players:")
+    def enter_Names2p(self):
         names = playr.getNames()
-        
+        disp.showPlayers(names)
         name = input("Enter the name of player1(note: if you already have your name in the list, enter it): ")
         name2 = input("Enter the name of player2(note: if you already have your name in the list, enter it): ")
         playr.setName(name)
         playr.setName(name2)
+        playr.addCurrentScore(0, name)
+        playr.addCurrentScore(0, name2)
         playr.addCurrentNames(name, name2)
-        currentNames = playr.getCurrentNames()
+        self.startGame2p()
+        
         #method 
         # curent points for both players 0
+    
+    
+    def startGame2p(self):
+        currentScores = playr.getCurrentScore()
+        score1 = currentScores[0]
+        score2 = currentScores[1]
+        while score1 != 100 and score2 != 100:
+            self.ask_For_Rolls()
+        else:
+            winnerName = dise.getWinnerName()
+            disp.winner(winnerName)
+            whichPlayer = playr.getAmountOfRolls(winnerName)
+            rollAmount = dise.amountOfRolls(whichPlayer)
+            disp.gameSummary(winnerName, rollAmount)
+            
     
     
     def enter_Names1(self):
@@ -66,12 +84,25 @@ class Game_functionality:
 
     
     def ask_For_Rolls(self):
-        # get the current name list
-        # get the current point list
-        # self.view_Game_Prog_2(name1, points1, name2, points2)
+        currentNames = playr.getCurrentNames()
+        player1 = currentNames[0]
+        player2 = currentNames[1]
+        currentScores = playr.getCurrentScore()
+        score1 = currentScores[0]
+        score2 = currentScores[1]
+        disp.viewGameProg2(player1, score1, player2, score2)
         print(currentPlayer,"\n")
-        roll_num = input("Enter the number of dice-rolls you would like to do ('q' to quit, 'r' to restart): ")
-        # call for the roll method (get back numbers)
+        rollNum = input("Enter the number of dice-rolls you would like to do ('q' to quit, 'r' to restart): ")
+        if rollNum.upper() == "Q":
+            quit()
+        elif rollNum.upper() == "CHEAT":
+            self.cheat()
+        elif rollNum.upper() == "R":
+            self.restart()
+        elif not isinstance(rollNum, int):
+            print("You know that the number of rolls is a NUMBER right?")
+        else:
+            dise.roll(rollNum)
         if currentPlayer == "player1's turn":
             currentPlayer = "player2's turn"
         else:
@@ -79,9 +110,8 @@ class Game_functionality:
 
 
     def ask_For_Rolls1p(self):
-        # get the current name list
-        # get the current point list
-        # self.view_Game_Prog_2(name1, points1, name2, points2)
+        
+        
         print(currentPlayer1,"\n")
         roll_num = input("Enter the number of dice-rolls you would like to do ('q' to quit, 'r' to restart, 'cheat' to cheat): ")
         if roll_num.upper() == "Q":
