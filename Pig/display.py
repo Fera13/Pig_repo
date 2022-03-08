db9abc05faea2b86ea97f5125bf92869b7fa2cb2
Stuhren <alfredstyrenberg@gmail.com>
2022-03-08 17:56:04 +0100

    Adjusted main

--- Pig/display.py
+++ Pig/display.py
@@ -1,6 +1,5 @@
-import Game_functionality
 
-gameFunc = Game_functionality()
+
 class Display:
 
     def winner(self, name:str):
@@ -12,26 +11,26 @@ class Display:
         print(f'\n----------------------------------------------------------------------\n')
 
 
-    def gameMenu(self):
-        notCorrect = True
-        while notCorrect:
-            print(f'\n----------------------------------------------------------------------')
-            print('1:  Start a one player game')
-            print('2:  Start a two player game')
-            print('3:  Update existing player name')
-            print('4:  Delete existing player')
-            print('5:  View highscore')
-            print('6:  Show rules')
-            print('7:  Exit game')
-            print(f'----------------------------------------------------------------------\n')
-            try:
-                choice = input('Please enter your choice here: ')
-                if choice in [1,2,3,4,5,6,7]:
-                    gameFunc.handleMenuChoice(choice)
-                else:
-                    print('Please enter a number from the available options')
-            except TypeError:
-                print('You can only use numbers to choose an option')
+    #def gameMenu(self):
+        #notCorrect = True
+        #while notCorrect:
+            #print(f'\n----------------------------------------------------------------------')
+            #print('1:  Start a one player game')
+            #print('2:  Start a two player game')
+            #print('3:  Update existing player name')
+            #print('4:  Delete existing player')
+            #print('5:  View highscore')
+            #print('6:  Show rules')
+            #print('7:  Exit game')
+            #print(f'----------------------------------------------------------------------\n')
+            #try:
+                #choice = input('Please enter your choice here: ')
+                #if choice in [1,2,3,4,5,6,7]:
+                    #gameFunc.handleMenuChoice(choice)
+                #else:
+                    #print('Please enter a number from the available options')
+            #except TypeError:
+                #print('You can only use numbers to choose an option')
 
 
     def displayGameRules(self):
--- Pig/main.py
+++ Pig/main.py
@@ -1,3 +1,24 @@
 from Game_functionality import *
 
 gf = Game_functionality()
+
+def gameMenu(self):
+        notCorrect = True
+        while notCorrect:
+            print(f'\n----------------------------------------------------------------------')
+            print('1:  Start a one player game')
+            print('2:  Start a two player game')
+            print('3:  Update existing player name')
+            print('4:  Delete existing player')
+            print('5:  View highscore')
+            print('6:  Show rules')
+            print('7:  Exit game')
+            print(f'----------------------------------------------------------------------\n')
+            try:
+                choice = input('Please enter your choice here: ')
+                if choice in [1,2,3,4,5,6,7]:
+                    gf.handleMenuChoice(choice)
+                else:
+                    print('Please enter a number from the available options')
+            except TypeError:
+                print('You can only use numbers to choose an option')
