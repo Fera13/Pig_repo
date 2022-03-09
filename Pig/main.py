ca81593813da341e899804152c205bb1be75268a
Stuhren <alfredstyrenberg@gmail.com>
2022-03-09 23:53:08 +0100

    Fixed main

--- Pig/main.py
+++ Pig/main.py
@@ -1,16 +1,9 @@
-from high_score import *
 from display import *
-from player import *
-from dice import *
-from main import *
 from game_functionality import *
 
 
 gf = Game_functionality()
-hs = High_score()
 disp = Display()
-playr = Player()
-dise = Dice()
 
 
 def main():
