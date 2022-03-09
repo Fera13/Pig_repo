d748831e7a795aeea326afd5cc050b30974a69eb
Emil <emil@gnaget.se>
2022-03-09 23:54:15 +0100

    Merge branch 'main' of https://github.com/Fera13/Pig_repo

---
+++ Pig-test/Test_High_score.py
@@ -0,0 +1,29 @@
+import unittest
+
+from test import High_score
+
+class testGameFunctionality(unittest.TestCase):
+    
+    def test_view_Highscores(self):
+        with self.assertRaises(TypeError): High_score.view_HighScores("hi")
+        with self.assertRaises(TypeError): High_score.view_HighScores(dict[int: str])
+    
+    def test_add_Compare_Highscores(self):
+        d1 = {"Farah": 13}
+        self.assertDictEqual(High_score.add_Compare_Highscores("Farah", 13), d1)
+        d2 = {"shhh": 3, "Farah": 13}
+        self.assertDictEqual(High_score.add_Compare_Highscores("shhh", 3), d2)
+        d3 = {"shhh": 3, "maha": 7, "Farah": 13}
+        self.assertDictEqual(High_score.add_Compare_Highscores("maha", 7), d3)
+        d4 = {"kamil": 1, "shhh": 3, "maha": 7, "Farah": 13}
+        self.assertDictEqual(High_score.add_Compare_Highscores("kamil", 1), d4)
+        d5 = {"kamil": 1, "shhh": 3, "suha": 4, "maha": 7, "Farah": 13}
+        self.assertDictEqual(High_score.add_Compare_Highscores("suha", 4), d5)
+        d6 = {"kamil": 1, "saby": 2, "shhh": 3, "suha": 4, "maha": 7}
+        self.assertDictEqual(High_score.add_Compare_Highscores("saby", 2), d6)
+        
+        with self.assertRaises(TypeError): High_score.add_Compare_Highscores([], 2)
+        with self.assertRaises(TypeError): High_score.add_Compare_Highscores("hi", "world")
+
+if __name__ == '__main__':
+    unittest.main()
