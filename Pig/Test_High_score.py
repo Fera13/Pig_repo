4259ad9d265c752892bc08af57e655da58bd562f
Emil <emil@gnaget.se>
2022-03-10 17:58:41 +0100

    fixed flake8 errors in test highscore

--- Pig/Test_High_score.py
+++ Pig/Test_High_score.py
@@ -1,22 +1,22 @@
+"""This class tests highscore class."""
 import unittest
 from high_score import High_score
-""" """
 
 hs = High_score()
 
 
 class testHighScore(unittest.TestCase):
-    """Tests the high score class"""
+    """Tests the high score class."""
 
     def test_view_Highscores_wrongType(self):
         """Tests that an error will be raised if the wrong type of arguments \
-            is entered"""
+            is entered."""
         with self.assertRaises(ValueError):
             hs.view_HighScores("hi")
 
     def test_add_Compare_Highscores(self):
         """Tests that the high score dictionary will be returned after adding \
-            or replacing keys and values to it"""
+            or replacing keys and values to it."""
         d1 = {"Farah": 13, "Developer": 9001}
         self.assertDictEqual(hs.add_Compare_Highscores("Farah", 13), d1)
         d2 = {"shhh": 3, "Farah": 13, "Developer": 9001}
@@ -30,31 +30,31 @@ class testHighScore(unittest.TestCase):
 
     def test_get_HighScore_Dic(self):
         """Tests that the high score dictionary will be returned when asking\
-             to get it"""
+             to get it."""
         d1 = {"kamil": 1, "shhh": 3, "suha": 4, "maha": 7, "Farah": 13}
         self.assertDictEqual(hs.get_HighScore_Dic(), d1)
 
     def test_add_Compare_Highscores_wrongName(self):
         """Tests that an error will be raised if the wrong type of arguments\
-             is entered"""
+             is entered."""
         with self.assertRaises(ValueError):
             hs.add_Compare_Highscores([], 2)
 
     def test_add_Compare_Highscores_wrongscore(self):
         """Tests that an error will be raised if the wrong type of arguments\
-             is entered"""
+             is entered."""
         with self.assertRaises(ValueError):
             hs.add_Compare_Highscores("hi", "world")
 
     def test_update_High_Score(self):
         """Tests that the updated high score dictionary will be returned after\
-             changing a key in it"""
+             changing a key in it."""
         d1 = {"kamil": 1, "Fera": 3, "suha": 4, "maha": 7, "Farah": 13}
         self.assertDictEqual(hs.update_High_Score("shhh", "Fera"), d1)
 
     def test_update_High_Score_wrongType(self):
         """Tests that an error will be raised if the wrong type of arguments\
-             is entered"""
+             is entered."""
         with self.assertRaises(ValueError):
             hs.add_Compare_Highscores(3, "world")
         with self.assertRaises(ValueError):
